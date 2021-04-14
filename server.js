e68cbbb433e8d7bcb834902a58e1eb050c9ab9ff
Anthony Castillo <anthonycastillo821@gmail.com>
2021-04-14 13:36:15 -0500

    Delete logic fixed, cleaned up code and comments

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 // Dependencies
-
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const dbfile = require('./db/db.json')
+let dbfile = require('./db/db.json')
 const uniqid = require('uniqid');
 
-
-
 // Sets up the Express App
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -18,32 +14,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
 
-
-// Basic route that sends the user first to the AJAX Page
+// Basic routing for html serving
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 
-
-// Displays a single character, or returns false
+// Retrieves db.json file for note display
 app.get('/api/notes', (req, res) => {
     
     return res.json(dbfile);
 });
 
-
-// Create New Characters - takes in JSON input
+// Create New Note - takes in JSON input
 app.post('/api/notes', (req, res) => {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
 
     const newNote = req.body;
-    
     newNote.id = uniqid()
 
     dbfile.push(newNote);
-
     noteString = JSON.stringify(dbfile);
 
     fs.writeFile('./db/db.json', noteString, (err) =>
@@ -53,29 +41,20 @@ app.post('/api/notes', (req, res) => {
     res.json(noteString);
 });
 
-
-app.delete('/api/notes/:id', (req, res) => {
-    req.params.id;
-
-
-
-    return res.json(dbfile);
-});
-
+// Takes in specific note for deletion with note id and rewrites db.json
 app.delete('/api/notes/:id', (req, res)  => {
- let id = req.params.id ;
-    fs.readFile("./db/db.json", 'utf8', (err,data) => {
-        
-        data = JSON.parse( data );
+ 
+    let id = req.params.id ;
+    console.log(id)
 
-        delete data["user" + id];
+    dbfile = dbfile.filter(note => note.id !== id ) 
+    noteString = JSON.stringify(dbfile);
 
-        console.log( JSON.stringify(data) );
-        res.status(200);
+    fs.writeFile('./db/db.json', noteString, (err) =>
+    err ? console.error(err) : console.log('Success!')
+    );
 
-        return res.send("Removed");
-    });
+    return res.send("Removed");
 })
-
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
